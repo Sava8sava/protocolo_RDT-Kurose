23ae5d8505ead92f3e207c0d5c3493b25ecbfbee
sava8sava <supremomelao@gmail.com>
2025-10-08 18:26:58 -0300

    Criação do servidor em C++ pelo Saleh, já funcionando com os clientes em python

--- client.py
+++ client.py
@@ -1,25 +1,29 @@
 import socket
 
 ROUTER_HOST = '127.0.0.1' #endereço ip 
-ROUTER_PORT = 8000
+ROUTER_PORT = 12345
 BUFFER_SIZE = 1024
 
 def start_client():
     mensagem = input("Cliente: Digite a mensagem para o servidor: ")
 
-    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
+    with socket.socket(socket.AF_INET,socket.SOCK_DGRAM) as sock:
         try:
-            #cliente se conecta ao roteador
-            sock.connect((ROUTER_HOST,ROUTER_PORT))
+            #ativar essa linha caso mudem pra TCP
+            #sock.connect((ROUTER_HOST,ROUTER_PORT)) 
         
-            sock.sendall(mensagem.encode('utf-8'))
+            sock.sendto(mensagem.encode('utf-8'),(ROUTER_HOST,ROUTER_PORT))
             print("Cliente: mensagem enviada ao servidor")
             
+            #aguarda resposta
+            sock.settimeout(5)
             #recebe a resposta do servidor 
-            data = sock.recv(BUFFER_SIZE)
-            print(f"Cliente: Resposta recebida do server: {data.decode('utf-8')}")
-        except ConnectionRefusedError:
-            print("Cliente: Erro de conexão")
+
+            data , addr = sock.recvfrom(BUFFER_SIZE)
+            print(f"Cliente: Resposta recebida do server({addr}): {data.decode('utf-8')}")
+
+        except socket.timeout:
+            print("Cliente: Limite de tempo atingindo")
         except Exception as e:
             print(f"Cliente: Erro: '{e}'")
 
